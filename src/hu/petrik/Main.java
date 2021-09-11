48ceb804f84c26d4dd3f18cd7fba0922206bda57
Levente Ibranyi <ibranyilevente@gmail.com>
2021-09-11 11:18:43 +0200

    Ötös feladat

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,8 +1,38 @@
 package hu.petrik;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void otosFeladat()
+    {
+        int a, b;
+        char kerTer;
+        System.out.print("Add meg a téglalap 'a' oldalát: ");
+        a = sc.nextInt();
+        System.out.print("Add meg a téglalap 'b' oldalát: ");
+        b = sc.nextInt();
+
+        System.out.print("Kerületet vagy területet számoljak: ");
+        kerTer = sc.next().toLowerCase().charAt(0);
+
+        if (kerTer == 'k')
+        {
+            System.out.printf("A téglalap kerülete: %d\n", 2 * (a + b));
+        }
+        else if (kerTer == 't')
+        {
+            System.out.printf("A téglalap területe: %d\n", a * b);
+        }
+        else
+        {
+            System.out.println("Nincs ilyen lehetőség!");
+        }
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        otosFeladat();
+
     }
 }
